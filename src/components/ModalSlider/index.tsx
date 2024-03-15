7e98aad8487180b3aa7f2031c7f6e4cda5f6fabf
Vladimir De Vico <82602547+vovkavovic@users.noreply.github.com>
2024-03-15 16:57:41 +0000

    feat(core): add view animation

--- src/components/ModalSlider/index.tsx
+++ src/components/ModalSlider/index.tsx
@@ -1,8 +1,10 @@
 import { useViewMeasureContext } from "@/providers/ViewMeasuresProvider";
 import { CSSProperties, useEffect, useRef } from "react";
+import { usePrevious } from "react-use";
 import useMeasure from "@/hooks/useMeasure";
-import { motion } from "framer-motion";
+import { AnimationSequence, motion, useAnimate } from "framer-motion";
 import { useViewParamContext } from "@/providers/ViewParamProvider";
+import { easings } from "@/constants/easings";
 
 export default function ModalSlider() {
   const initialContainerStyles = useRef<CSSProperties>({});
@@ -11,6 +13,7 @@ export default function ModalSlider() {
   const { updateSliderMeasure, listMeasure, sliderMeasure } =
     useViewMeasureContext();
   const { viewParam } = useViewParamContext();
+  const previousViewParam = usePrevious(viewParam);
 
   const [sliderStateRef, { x, y, width, height, top, right, bottom, left }] =
     useMeasure();
@@ -18,6 +21,8 @@ export default function ModalSlider() {
   useEffect(() => {
     updateSliderMeasure({ x, y, width, height, top, right, bottom, left });
   }, [x, y, width, height, top, right, bottom, left]);
+
+  const [scope, animate] = useAnimate();
 
   // Set initial modal bg overlay styles
   initialModalBgOverlayStyles.current =
@@ -49,8 +54,86 @@ export default function ModalSlider() {
           // opacity: 0,
         };
 
+  useEffect(() => {
+    const animations =
+      viewParam === "slider"
+        ? [
+            [
+              "#modal-bg-overlay",
+              { y: "0%", rotate: 0, scale: 1.25 },
+              { ease: easings.easeInOutQuint, duration: 1.2, delay: 1.2 },
+            ],
+            [
+              "#main-slider-container",
+              {
+                // From To logic
+                top: [listMeasure.top, sliderMeasure.top],
+                left: [listMeasure.left, sliderMeasure.left],
+                width: [listMeasure.width, sliderMeasure.width],
+                height: [listMeasure.height, sliderMeasure.height],
+              },
+              {
+                ease: easings.easeInOutQuint,
+                duration: 1.2,
+                delay: 0.013,
+                at: "-1.2",
+              },
+            ],
+            [
+              "#main-slider-container",
+              {
+                opacity: 1,
+              },
+              { duration: 0.01, delay: 1.4, at: "-2.4" },
+            ],
+            [
+              "#main-slider-container",
+              {
+                opacity: 0,
+              },
+              { duration: 0.01, delay: 1.2, at: "2.4" },
+            ],
+          ]
+        : [
+            [
+              "#modal-bg-overlay",
+              { y: "125%", rotate: -5, scale: 1.25 },
+              { ease: easings.easeInOutQuint, duration: 1.2 },
+            ],
+            [
+              "#main-slider-container",
+              {
+                // From To logic
+                top: [sliderMeasure.top, listMeasure.top],
+                left: [sliderMeasure.left, listMeasure.left],
+                width: [sliderMeasure.width, listMeasure.width],
+                height: [sliderMeasure.height, listMeasure.height],
+              },
+              { ease: easings.easeInOutQuint, duration: 1.2, at: "-1.2" },
+            ],
+            [
+              "#main-slider-container",
+              {
+                opacity: 1,
+              },
+              { duration: 0.01, delay: 0, at: "-2.4" },
+            ],
+            [
+              "#main-slider-container",
+              {
+                opacity: 0,
+              },
+              { duration: 0.01, delay: 1.2, at: "2.4" },
+            ],
+          ];
+    animate([...animations] as AnimationSequence);
+  }, [viewParam]);
+
   return (
-    <div className="fixed inset-0 w-full h-full z-40 flex items-center justify-center">
+    <div
+      ref={scope}
+      className="fixed inset-0 w-full h-full z-40 flex items-center justify-center"
+    >
       <div
         ref={sliderStateRef}
         className="bg-green-500/50 z-20 aspect-[1152/648] h-[calc(100%-216px)] w-[74%] rounded-[16px]"
---
+++ src/constants/easings.ts
@@ -0,0 +1,9 @@
+export const easings: Record<string, [number, number, number, number]> = {
+  defaultEasing: [0.0, 1.0, 0.0, 1.0],
+  easeInQuint: [0.64, 0, 0.78, 0],
+  easeOutQuint: [0.22, 1, 0.36, 1],
+  easeInOutQuint: [0.83, 0, 0.17, 1],
+  //CUSTOM
+  ease1In3Out: [0.47, 0.15, 0.22, 1],
+  easeOutCustomOne: [0, 0, 0, 1],
+};
